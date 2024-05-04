7406ebb4fcefd97796ecb9cc50dc57cacf301a5b
Lyle Dean <dean@lyle.dev>
2024-05-04 22:58:52 +0100

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -12,8 +12,6 @@ pub fn main() !void {
         std.debug.print("Usage: {s} <filename>\n", .{args[0]});
         return;
     }
-
-    // The first argument after the program name is assumed to be the filename
     const filename = args[1];
 
     var file = try std.fs.cwd().openFile(filename, .{});
@@ -27,6 +25,8 @@ pub fn main() !void {
 
     var line_count: usize = 0;
     var byte_count: usize = 0;
+
+    // todo: reorder this to do in one pass
     while (true) {
         reader.streamUntilDelimiter(arr.writer(), '\n', null) catch |err| switch (err) {
             error.EndOfStream => break,
@@ -39,18 +39,14 @@ pub fn main() !void {
     }
 
     var i: usize = 0;
+    var start: usize = 0;
 
     while (i < arr.items.len) {
         if (i % 16 == 0) {
             if (i != 0) {
+                start = i;
                 std.debug.print(" ", .{});
-                for (arr.items[i - 16 .. i]) |c| {
-                    if (c >= 0x20 and c <= 0x7E) {
-                        std.debug.print("{c}", .{c});
-                    } else {
-                        std.debug.print(".", .{});
-                    }
-                }
+                printBytesToAscii(arr.items[i - 16 .. i]);
                 std.debug.print("\n", .{});
             }
             std.debug.print("{x:0>8}: ", .{i});
@@ -60,6 +56,28 @@ pub fn main() !void {
         std.debug.print("{x:02} ", .{hexString});
         i += 2;
     }
+
+    const end = i;
+
+    while (i % 16 != 0) {
+        std.debug.print("  ", .{});
+        i += 1;
+        if (i % 2 == 0) {
+            std.debug.print(" ", .{});
+        }
+    }
+
+    std.debug.print(" ", .{});
+    printBytesToAscii(arr.items[start..end]);
     std.debug.print("\n", .{});
 }
 
+fn printBytesToAscii(out: []u8) void {
+    for (out) |c| {
+        if (c >= 0x20 and c <= 0x7E) {
+            std.debug.print("{c}", .{c});
+        } else {
+            std.debug.print(".", .{});
+        }
+    }
+}
