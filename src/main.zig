e998e7a97889c6b91ac096845bb91b654c1dfaeb
Lyle Dean <dean@lyle.dev>
2024-05-04 22:44:53 +0100

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,18 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    var file = try std.fs.cwd().openFile("/Users/lyledean/compilers/cyclang/bin/main", .{});
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    if (args.len < 2) {
+        std.debug.print("Usage: {s} <filename>\n", .{args[0]});
+        return;
+    }
+
+    // The first argument after the program name is assumed to be the filename
+    const filename = args[1];
+
+    var file = try std.fs.cwd().openFile(filename, .{});
     defer file.close();
 
     var buffered = std.io.bufferedReader(file.reader());
@@ -32,13 +43,23 @@ pub fn main() !void {
     while (i < arr.items.len) {
         if (i % 16 == 0) {
             if (i != 0) {
+                std.debug.print(" ", .{});
+                for (arr.items[i - 16 .. i]) |c| {
+                    if (c >= 0x20 and c <= 0x7E) {
+                        std.debug.print("{c}", .{c});
+                    } else {
+                        std.debug.print(".", .{});
+                    }
+                }
                 std.debug.print("\n", .{});
             }
             std.debug.print("{x:0>8}: ", .{i});
         }
-        const hexString = std.fmt.fmtSliceHexLower(arr.items[i .. i + 2]);
+        const hexSlice = arr.items[i .. i + 2];
+        const hexString = std.fmt.fmtSliceHexLower(hexSlice);
         std.debug.print("{x:02} ", .{hexString});
         i += 2;
     }
     std.debug.print("\n", .{});
 }
+
